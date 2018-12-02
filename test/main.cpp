5e1b7dc5b311aaf1631a67ae56011dc3cc5fe798
taidalab <zdajun1988@163.com>
2018-12-02 22:10:09 +0800

    update

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,12 @@
-#include <stdio.h>
+/*lint ¨Celib(96) */
+#include "stdio.h"
 
 void main(void)
 {
-	printf("hello pclint/n");
+//	printf("hello pclint/n");
+//	int a[10];
+//	int k;
+//	k = 10;
+//	return a[k]; // Warning 415
+	
 }
