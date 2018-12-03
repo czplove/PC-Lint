aa75c1795f45e65560152ad984543f7109d14600
taidalab <zdajun1988@163.com>
2018-12-03 14:59:55 +0800

    增加了缩进检查,还有一些特殊抑制错误告警

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,15 @@
-/*lint ¨Celib(96) */
-#include "stdio.h"
+//#include <stdio.h>
 
 void main(void)
 {
 //	printf("hello pclint/n");
 //	int a[10];
-//	int k;
-//	k = 10;
+	int k;
+	k = 10;
 //	return a[k]; // Warning 415
-	
+	if(k > 10)
+	k = 1;
+	else
+		k = 0;
+    
 }
